59fb91b5d9b454ea8b406bf20b28e5da2cc14733
Nicolas Ayala <nicoatnico.com@gmail.com>
2021-10-16 19:19:37 -0700

    Updated files.

--- tree_select.js
+++ tree_select.js
@@ -25,5 +25,27 @@ class node{
         this.function = 0;
         this.x = 0;
         this.y = 0;
+
+
     }
 }
+
+class func{
+    constructor(name){
+        this.name = name
+    }
+}
+
+var function_table = Array.apply(null, Array(10)).map(function () {})
+
+function_table[0] = new func("add")
+function_table[1] = new func("subtract")
+function_table[2] = new func("scale")
+function_table[3] = new func("full")
+function_table[4] = new func("amass")
+function_table[5] = new func("softmax")
+function_table[6] = new func("hardmax")
+function_table[7] = new func("max")
+function_table[8] = new func("convolution")
+function_table[9] = new func("squared dist")
+
