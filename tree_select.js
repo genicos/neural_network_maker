0ada6f7d5c7a26588aa9db99feaeda821bace544
Nicolas Ayala <nicoatnico.com@gmail.com>
2021-10-17 01:31:36 -0700

    Updated files.

--- tree_select.js
+++ tree_select.js
@@ -10,31 +10,31 @@ example_net.add_tensor(new Tensor())
 example_net.add_tensor(new Tensor())
 example_net.add_tensor(new Tensor())
 
-example_net.output_tensors.push(0)
-example_net.tensors[0].function = function_table[5];
-example_net.tensors[0].parent_1 = 1
-example_net.param_tensors.push(1)
+example_net.add_operator(new Operator())
+example_net.add_operator(new Operator())
+example_net.add_operator(new Operator())
+example_net.add_operator(new Operator())
+example_net.add_operator(new Operator())
 
-example_net.tensors[1].function = function_table[3]
-example_net.tensors[1].parent_1 = 2;
-example_net.tensors[1].parent_2 = 3;
-example_net.param_tensors.push(2)
+example_net.operators[0].func = 7
+example_net.operators[0].outputs.push(0)
+example_net.operators[0].inputs.push(1)
 
-example_net.tensors[3].function = function_table[3]
-example_net.tensors[3].parent_1 = 4;
-example_net.tensors[3].parent_2 = 5;
-example_net.param_tensors.push(4)
+example_net.operators[1].func = 5
+example_net.operators[1].outputs.push(2)
+example_net.operators[1].inputs.push(3)
 
-example_net.tensors[5].function = function_table[8]
-example_net.tensors[5].parent_1 = 6;
-example_net.tensors[5].parent_2 = 7;
-example_net.param_tensors.push(6)
+example_net.operators[2].func = 5
+example_net.operators[2].outputs.push(4)
+example_net.operators[2].inputs.push(5)
 
-example_net.tensors[7].function = function_table[8]
-example_net.tensors[7].parent_1 = 8;
-example_net.tensors[7].parent_2 = 9;
-example_net.param_tensors.push(8)
-example_net.input_tensors.push(9)
+example_net.operators[3].func = 10
+example_net.operators[3].outputs.push(6)
+example_net.operators[3].inputs.push(7)
+
+example_net.operators[4].func = 10
+example_net.operators[4].outputs.push(8)
+example_net.operators[4].inputs.push(9)
 
 
 
