3dbeabbf3154878ced232ed0fc2fe2dad59f196d
Nicolas Ayala <nicoatnico.com@gmail.com>
2021-10-17 00:55:28 -0700

    Created first draft of network expand

--- define_objects.js
+++ define_objects.js
@@ -16,9 +16,42 @@ class Network{
     }
     expand(){
         //assumes that network is alright, ok, and doin well
+        //inner networks should never have parameter nodes
         for(let i = 0; i < this.operators.length; i++){
             if(this.operators[i].func == 0 && this.operators[i].network){
-                
+                var inner_net = this.operators[i].network;
+                inner_net.expand();
+                var offset = this.tensors.length - inner_net.input_tensors.length - inner_net.output_tensors.length;
+                for(let k = 0; k < inner_net.tensors.length; k++){
+                    if(!inner_net.input_tensors.includes(k) && !inner_net.output_tensors.includes(k)){
+                        this.add_tensor(inner_net.tensors[k]);
+                    }
+                }
+                for(let k = 0; k < inner_net.operators.length; k++){
+                    for(let j = 0; j < inner_net.operators[k].inputs.length; j++){
+                        var outer_inp = inner_net.inputs.indexOf(inner_net.operators[k].inputs[j])
+                        var outer_out = inner_net.outputs.indexOf(inner_net.operators[k].inputs[j])
+                        if(outer_inp != -1){
+                            inner_net.operators[k].inputs[j] = this.operators[i].inputs[outer_inp]
+                        }else if(outer_out != -1){
+                            inner_net.operators[k].inputs[j] = this.operators[i].outputs[outer_out]
+                        }else{
+                            inner_net.operators[k].inputs[j] += offset
+                        }
+                    }
+                    for(let j = 0; j < inner_net.operators[k].outputs.length; j++){
+                        var outer_inp = inner_net.inputs.indexOf(inner_net.operators[k].outputs[j])
+                        var outer_out = inner_net.outputs.indexOf(inner_net.operators[k].outputs[j])
+                        if(outer_inp != -1){
+                            inner_net.operators[k].outputs[j] = this.operators[i].inputs[outer_inp]
+                        }else if(outer_out != -1){
+                            inner_net.operators[k].outputs[j] = this.operators[i].outputs[outer_out]
+                        }else{
+                            inner_net.operators[k].outputs[j] += offset
+                        }
+                    }
+                    this.add_operator(inner.operators[k])
+                }
             }
         }
     }
