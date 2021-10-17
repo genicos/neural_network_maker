35aa9f77c45e7ade24085e22055daf4f2e5a4c58
Nicolas Ayala <nicoatnico.com@gmail.com>
2021-10-16 23:44:56 -0700

    Updated files.

--- define_objects.js
+++ define_objects.js
@@ -1,36 +1,42 @@
 
 class Network{
     constructor(){
-        this.num_nodes = 0
-        this.nodes = []
+        this.tensors = [] //actual tensor objects
+        this.operators = [] //actual operator objects
 
-        this.input_nodes = []
-        this.param_nodes = []
-        this.output_nodes = []
-
-        this.x = 0;
-        this.y = 0;
+        this.input_tensors = []
+        this.param_tensors = []
+        this.output_tensors = []
     }
 
-    addNode(n){
-        this.num_nodes += 1
-        this.nodes.push(n)
-    }
 }
 
-class Node{
+class Tensor{
     constructor() {
-        this.parent_1 = null;
-        this.parent_2 = null;
+        this.scalar = false
 
-        this.num_children = 0;
-        this.children = []
+        this.form = []
 
-        this.function = null;
         this.x = 0;
         this.y = 0;
     }
 }
+
+class Operator{
+    constructor(){
+        this.inputs = []
+        this.outputs = []
+
+        this.func = 0
+        this.network = null
+    }
+}
+
+
+
+
+
+
 
 class Func{
     constructor(name){
@@ -38,16 +44,18 @@ class Func{
     }
 }
 
-var function_table = Array.apply(null, Array(10)).map(function () {})
+var function_table = Array.apply(null, Array(12)).map(function () {})
 
-function_table[0] = new Func("add")
-function_table[1] = new Func("subtract")
-function_table[2] = new Func("scale")
-function_table[3] = new Func("full")
-function_table[4] = new Func("amass")
-function_table[5] = new Func("softmax")
-function_table[6] = new Func("hardmax")
-function_table[7] = new Func("max")
-function_table[8] = new Func("convolution")
-function_table[9] = new Func("squared dist")
+function_table[0] = new Func("abstraction")
+function_table[1] = new Func("identity")
+function_table[2] = new Func("add")
+function_table[3] = new Func("subtract")
+function_table[4] = new Func("scale")
+function_table[5] = new Func("full")
+function_table[6] = new Func("amass")
+function_table[7] = new Func("softmax")
+function_table[8] = new Func("hardmax")
+function_table[9] = new Func("max")
+function_table[10] = new Func("convolution")
+function_table[11] = new Func("squared dist")
 
